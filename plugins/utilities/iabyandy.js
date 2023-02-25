eacb25e46c6271beed9ed07865cd6be8c9bc3eb2
WhatsBot6 <ricadostphard08@gmail.com>
2023-02-25 17:55:04 -0500

    HAITIAN

---
+++ plugins/utilities/iabyandy.js
@@ -0,0 +1,20 @@
+exports.run = {
+   usage: ['ia'],
+   use: 'ia',
+   category: 'utilities',
+   async: async (m, {
+      client,
+      text,
+      isPrefix,
+      command
+   }) => {
+      try {
+         if (!text) throw `*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ ${usedPrefix + command}series 2022  netflix*\n*◉ ${usedPrefix + command} write a js code*`
+try {
+client.reply(`*Fon ti tann me bro*`)
+         let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
+         let hasil = await tiores.json()
+client.reply(`${hasil.result}`.trim())
+} catch {
+throw `*𝙴𝚁𝚁𝙾𝚁*`
+}}
