cdc5d4fb3bffc8931d052ad8bc1c475dc4e1d99b
botpou <112909419+botpou@users.noreply.github.com>
2023-02-25 19:19:08 -0500

    Add files via upload

---
+++ plugins/chatgpt.js
@@ -0,0 +1,24 @@
+exports.run = {
+   usage: ['ia', 'ai'],
+   async: async (m, {
+      client,
+      args,
+      isPrefix,
+      command
+   }) => {
+      try {
+         if (!text) throw `*enter a request or an order to use the chatgpt*\n\n*—◉ 𝙴xample*\n*◉ .ia series 2022  netflix*\n*◉ .ia write a js code*`
+try {
+client.reply(`*Fon ti tann me bro*`)
+         let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
+         let hasil = await tiores.json()
+client.reply(`${hasil.result}`.trim())
+} catch {
+throw `*𝙴𝚁𝚁𝙾𝚁*`
+        }
+      }
+   },
+   error: false,
+   cache: true,
+   location: __filename
+}
